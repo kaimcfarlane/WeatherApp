8afccde1900da0ea9ae262b03289b0930a361ce8
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-08-06 11:10:55 -0400

    Added Switches for Settings

--- Weatherapp.js
+++ Weatherapp.js
@@ -569,3 +569,20 @@ var slideIndex = 0;
     // var tName = coordValue;
     // console.log (String(tName).match(/\d+/g));
 
+    //for setting stiwches
+
+    var toggle = document.getElementById('container');
+var toggleContainer = document.getElementById('toggle-container');
+var toggleNumber;
+
+toggle.addEventListener('click', function() {
+	toggleNumber = !toggleNumber;
+	if (toggleNumber) {
+		toggleContainer.style.clipPath = 'inset(0 0 0 50%)';
+		toggleContainer.style.backgroundColor = '#D74046';
+	} else {
+		toggleContainer.style.clipPath = 'inset(0 50% 0 0)';
+		toggleContainer.style.backgroundColor = 'dodgerblue';
+	}
+	console.log(toggleNumber)
+});
